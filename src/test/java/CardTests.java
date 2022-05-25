3b6558e37a9d6d398caab62ec02a23e379d20443
Artem-Mv <Stimul113@gmail.com>
2022-05-25 20:17:09 +0700

    fix wait element, add check date

--- src/test/java/CardTests.java
+++ src/test/java/CardTests.java
@@ -1,20 +1,29 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CardTests {
     @Test
-    void cardOrderPositiveTest() throws InterruptedException {
+    void cardOrderPositiveTest(){
+        Helper helper = new Helper();
+
         open("http://localhost:9999/");
+
         $x("//input[@placeholder='Город']").setValue("Кемерово");
-        //  дата выставляется автоматом
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("(//input[@class='input__control'])[2]").setValue(helper.datePlusDay(3));
         $x("//input[@name='name']").setValue("Фамилия Имя");
         $x("//input[@name='phone']").setValue("+79999999999");
         $x("//label[@data-test-id='agreement']").click();
         $x("//span[@class='button__text']").click();
 
-        Thread.sleep(10000);
-        $x("//div[@data-test-id='notification']").shouldBe(Condition.visible);
+        $x("//div[@data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + helper.datePlusDay(3)),
+                        Duration.ofSeconds(15));
     }
 }
