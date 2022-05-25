fc933dbf8d36d599bf7fcc7b1670f834e4303644
Artem-Mv <91866848+Artem-Mv@users.noreply.github.com>
2022-05-25 20:40:47 +0700

    Update CardTests.java

--- src/test/java/CardTests.java
+++ src/test/java/CardTests.java
@@ -15,7 +15,7 @@ public class CardTests {
 
         $x("//input[@placeholder='Город']").setValue("Кемерово");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $x("(//input[@class='input__control'])[2]").setValue(helper.datePlusDay(3));
+        $x("(//input[@class='input__control'])[2]").setValue(helper.datePlusDay(4));
         $x("//input[@name='name']").setValue("Фамилия Имя");
         $x("//input[@name='phone']").setValue("+79999999999");
         $x("//label[@data-test-id='agreement']").click();
@@ -23,7 +23,7 @@ public class CardTests {
 
         $x("//div[@data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + helper.datePlusDay(3)),
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + helper.datePlusDay(4)),
                         Duration.ofSeconds(15));
     }
 }
